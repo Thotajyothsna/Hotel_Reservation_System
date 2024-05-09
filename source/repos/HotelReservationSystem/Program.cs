f7a57d882ec58159af7e8f80797cdbcbca4ea82e
Thotajyothsna <thotajyothsna2001@gmail.com>
2024-05-09 12:45:35 +0530

    Displaying a welcome message to customers

--- source/repos/HotelReservationSystem/Program.cs
+++ source/repos/HotelReservationSystem/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace HotelReservationSystem
+{
+    class Program
+    {
+        public List<Hotel> hotels = new List<Hotel>();
+        public static void Main(string[] args)
+        {
+            DisplayMessage();
+
+        }
+
+        static void DisplayMessage()
+        {
+            Console.WriteLine("warm welcome to Hotel Reservation");
+            Console.WriteLine("We provide clean and safe rooms to our customers");
+        }
+    }
+}

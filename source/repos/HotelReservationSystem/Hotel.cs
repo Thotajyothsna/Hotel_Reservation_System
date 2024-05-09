9cae6a703899847f9c8e34c73b09f8a315126927
Thotajyothsna <thotajyothsna2001@gmail.com>
2024-05-09 15:43:51 +0530

    Added weekdays rate and weekends rate to Hotel

--- source/repos/HotelReservationSystem/Hotel.cs
+++ source/repos/HotelReservationSystem/Hotel.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace HotelReservationSystem
 {
-    internal class Hotel
+    public class Hotel
     {
+        public string Hotel_Name;
+        public int Hotel_Ratings;
+        public int Weekday_Rates;
+        public int Weekend_Rates;
+        public Hotel(string hotel_name,int hotel_rating,int weekday_rates,int weekend_rates)
+        { 
+            Hotel_Name = hotel_name;
+            Hotel_Ratings = hotel_rating;
+            Weekday_Rates = weekday_rates;
+            Weekend_Rates = weekend_rates;
+            
+        }
+        
     }
 }
